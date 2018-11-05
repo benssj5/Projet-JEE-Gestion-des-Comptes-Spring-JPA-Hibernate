07096ad6a310501dd47e7dc369aa96c597419e0d
habibatou <habipenda93@gmail.com>
2018-11-05 19:46:09 +0000

    premier commit

--- VotreBanque/src/main/java/org/sid/dao/CompteRepository.java
+++ VotreBanque/src/main/java/org/sid/dao/CompteRepository.java
@@ -3,6 +3,12 @@ package org.sid.dao;
 import org.sid.entities.Compte;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/*
+**
+*HABI
+*
+*/
+
 public interface CompteRepository extends JpaRepository<Compte, String>{
 
 }
--- VotreBanque/src/main/java/org/sid/dao/OperationRepository.java
+++ VotreBanque/src/main/java/org/sid/dao/OperationRepository.java
@@ -7,6 +7,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+/*
+**
+*HABI
+*
+*/
+
 public interface OperationRepository extends JpaRepository<Operation, Long>{
 
 	@Query("select o from Operation o where o.compte.codeCompte =:x order by o.dateOperation desc")
