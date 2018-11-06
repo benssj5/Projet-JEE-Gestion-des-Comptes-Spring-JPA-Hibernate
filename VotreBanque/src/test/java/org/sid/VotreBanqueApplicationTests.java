17e2b52ff14bd85003e5ce5874261197149ea346
Ndeye-Ada <ndeyetouada@gmail.com>
2018-11-06 12:56:42 +0000

    1er commit

--- VotreBanque/src/test/java/org/sid/VotreBanqueApplicationTests.java
+++ VotreBanque/src/test/java/org/sid/VotreBanqueApplicationTests.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+//adama
+//adama
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
